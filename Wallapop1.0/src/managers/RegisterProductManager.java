0f74dd9a3f2a853a4ee1c09654c0d0a4b68c7ee9
tiw <100316953@alumnos.uc3m.es>
2016-11-01 11:30:53 +0000

    Cambios en insertar producto

--- Wallapop1.0/src/managers/RegisterProductManager.java
+++ Wallapop1.0/src/managers/RegisterProductManager.java
@@ -1,4 +1,5 @@
 package managers;
+
 import java.math.BigDecimal;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,38 +8,39 @@ import javax.servlet.http.HttpServletResponse;
 import logic.*;
 import model.*;
 
-public class RegisterProductManager extends Manager{
-	
+public class RegisterProductManager extends Manager {
+
 	public RegisterProductManager(HttpServletRequest pRequest, HttpServletResponse pResponse) {
 		super(pRequest, pResponse);
 	}
-	
-	public void Execute(){
-		Product producto=new Product();
-		ProductManager manager=new ProductManager();
-		producto.setDescription(((String)request.getParameter("descriptionproduct")));
-		producto.setName(((String)request.getParameter("productname")));
-		BigDecimal price=new BigDecimal(((String)request.getParameter("pricepproduct")));
+
+	public void Execute() {
+		Product producto = new Product();
+		ProductManager manager = new ProductManager();
+		producto.setDescription(((String) request.getParameter("descriptionproduct")));
+		producto.setName(((String) request.getParameter("productname")));
+		BigDecimal price = new BigDecimal(((String) request.getParameter("pricepproduct")));
 		producto.setPrice(price);
-		producto.setStatus(((String)request.getParameter("statusproduct")));
-		Category catproducto=new Category();
-		int catprod=Integer.parseInt((((String)request.getParameter("idcategory"))));
+		producto.setStatus(((String) request.getParameter("statusproduct")));
+		Category catproducto = new Category();
+		int catprod = Integer.parseInt((((String) request.getParameter("idcategory"))));
 		catproducto.setId(catprod);
-		User userproduct=new User();
-		int iduser=Integer.parseInt((((String)request.getParameter("iduser"))));
-		userproduct.setId(((iduser)));
-		
-		try{
+		catproducto.setName("electronica");
+		producto.setCategory(catproducto);
+		User userproduct = new User();
+		int iduser = Integer.parseInt((((String) request.getParameter("iduser"))));
+		userproduct.setId(iduser);
+		producto.setUser(userproduct);
+
+		try {
 			manager.createProduct(producto);
 			request.setAttribute("product", producto);
-			
-		}
-		 catch (Exception e) {
+
+		} catch (Exception e) {
 			// TODO: handle exception
-			 System.out.println(e.getMessage());
-			 
+			System.out.println(e.getMessage());
+
 		}
-		
-		
+
 	}
 }
