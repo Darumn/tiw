a72310c608ec24181311a2f5c4b40e097b315185
Darumn <mario.block46@gmail.com>
2016-11-06 12:46:27 +0000

    search manager

---
+++ Wallapop1.0/src/managers/SearchManager.java
@@ -0,0 +1,32 @@
+package managers;
+import model.*;
+import logic.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SearchManager extends Manager {
+
+	public SearchManager(HttpServletRequest pRequest, HttpServletResponse pResponse) {
+		super(pRequest, pResponse);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void Execute() {
+		// TODO Auto-generated method stub
+		try{
+			User user= this.session.getUser();
+			request.setAttribute("user", user);
+			request.getRequestDispatcher("./PerfilUsuario.jsp").include(request, response);
+
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	
+
+}
