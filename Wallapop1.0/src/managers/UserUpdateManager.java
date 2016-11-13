90fbde7670289038d422377e391046dbcf807f52
lga17 <100303546@alumnos.uc3m.es>
2016-11-13 16:53:37 +0000

    Hash update user

--- Wallapop1.0/src/managers/UserUpdateManager.java
+++ Wallapop1.0/src/managers/UserUpdateManager.java
@@ -37,7 +37,7 @@ public class UserUpdateManager extends Manager {
 			String name = request.getParameter("name");
 			String surname = request.getParameter("surname");
 			String email = request.getParameter("email");
-			String pass = request.getParameter("pass");
+			String pass = request.getParameter(RegisterUserManager.getHash((String)request.getParameter("pass"), "SHA-1"));
 			String city = request.getParameter("city");
 			
 			if(name != null && !name.equals("")){
@@ -113,4 +113,28 @@ public class UserUpdateManager extends Manager {
 		return ret;
 	}
 
+	public static String getHash(String password, String hashType) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest
+                    .getInstance(hashType);
+            byte[] array = md.digest(password.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100)
+                        .substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+ 
+   
+ 
+    
+    public static String sha1(String txt) {
+        return RegisterUserManager.getHash(txt, "SHA1");
+    }
+	
 }
